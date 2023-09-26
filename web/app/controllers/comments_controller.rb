d3810fe9a837035c0c59abb452b4d05dc5b6a6ff
ravihlb <ravi@deliverydireto.com.br>
2023-09-26 18:03:21 -0300

    fix(comments): show and edit actions

--- web/app/controllers/comments_controller.rb
+++ web/app/controllers/comments_controller.rb
@@ -65,6 +65,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.fetch(:comment, {})
+      params.require(:comment).permit(:body, :post_id)
     end
 end
