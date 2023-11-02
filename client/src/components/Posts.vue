e1e9cc703722a8c2cf68ab2c68607813263c8fde
ravihlb <ravi@deliverydireto.com.br>
2023-11-02 12:52:51 -0300

    feat(client): add Posts component

---
+++ client/src/components/Posts.vue
@@ -0,0 +1,36 @@
+<script lang='ts'>
+    import { defineComponent } from 'vue'
+    import api from '../http.ts'
+
+    export default defineComponent({
+        data() {
+            return {
+                posts: []
+            }
+        },
+
+        methods: {
+            async loadPosts() {
+                const response = await api.get('/posts')
+                this.posts = response.data
+            }
+        },
+
+        async mounted() {
+            await this.loadPosts()
+        }
+})
+
+</script>
+
+<template>
+    <h1>Posts</h1>
+
+    <div v-for="post in posts">
+        <div>
+            <h3>{{ post.title }}</h3>
+            <div>{{ post.body }}</div>
+        </div>
+    </div>
+
+</template>
--- client/src/router.ts
+++ client/src/router.ts
@@ -1,11 +1,17 @@
 import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Posts from './components/Posts.vue'
 
 const routes: RouteRecordRaw[] = [
     {
         path: '/hello',
         name: 'hello',
         component: HelloWorld
+    },
+    {
+        path: '/posts',
+        name: 'posts',
+        component: Posts
     }
 ]
 
