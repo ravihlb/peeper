6e00a66cc160fd8c802293f1515d15256e13e243
ravihlb <ravi@deliverydireto.com.br>
2023-11-03 20:02:39 -0300

    chore(App.vue): cleanup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,7 @@
 <script setup lang="ts">
 </script>
 
-<!-- Default template -->
-<!-- <template> -->
-<!--   <div> -->
-<!--     <a href="https://vitejs.dev" target="_blank"> -->
-<!--       <img src="/vite.svg" class="logo" alt="Vite logo" /> -->
-<!--     </a> -->
-<!--     <a href="https://vuejs.org/" target="_blank"> -->
-<!--       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /> -->
-<!--     </a> -->
-<!--   </div> -->
-<!--   <HelloWorld msg="Vite + Vue" /> -->
-<!-- </template> -->
-
 <template>
-  <nav>
-    <router-link to="/hello">Hello</router-link>
-  </nav>
   <router-view />
 </template>
 
