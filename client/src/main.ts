ca40611d2a66d47b9fe6b3b4350b9dadfe7e410c
ravihlb <ravi@deliverydireto.com.br>
2023-11-07 07:43:50 -0300

    feat(package): add vuetify

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,5 +2,14 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createApp } from 'vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).use(router).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
